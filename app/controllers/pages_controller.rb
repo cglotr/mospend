fc1dd31b633006b480044e57685375c516e0ff73
cglotr <amir.ariffin.920404@gmail.com>
2018-05-29 20:16:03 +0800

    logged in home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,5 +6,23 @@ class PagesController < ApplicationController
 
   def home
     redirect_to action: "hello" unless user_signed_in?
+
+    @spending_months = current_user.spending_months.includes(spendings: [:currency])
+
+    @spending_months = @spending_months.map do |s_month|
+      spending_currency_groups = Hash.new 0
+
+      s_month.spendings.each do |spending|
+        spending_currency_groups[spending.currency.code] += spending.cost
+      end
+
+      spending_currency_groups.sort_by { |currency, sum| currency }
+
+      {
+        month: s_month.month,
+        year: s_month.year,
+        spending_currency_groups: spending_currency_groups,
+      }
+    end
   end
 end
