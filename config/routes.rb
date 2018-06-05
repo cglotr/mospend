06976d906021520670aaeb296ac31bd27a715350
cglotr <amir.ariffin.920404@gmail.com>
2018-06-05 22:52:10 +0800

    change heading & user access paths

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   resources :spending_months
   resources :spendings
-  devise_for :users
+  devise_for :users, path: "",
+    path_names: { sign_in: "login", sign_out: "logout", sign_up: "signup" }
   get "hello", to: "pages#hello"
   root to: "pages#home"
 end
