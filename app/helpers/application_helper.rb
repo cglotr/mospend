37eb77459197ac4f71badf9a5e8ee9b391eccae3
cglotr <amir.ariffin.920404@gmail.com>
2018-06-10 10:55:04 +0800

    change mospend link to home/welcome

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def email_name(email)
+    email.split("@").first
+  end
+
   def heading(heading = "")
     return "mospend" if heading.empty?
     heading.downcase
