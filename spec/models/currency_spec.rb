80201cbf8a8eaeaeb2a2aa92934232ee0646cc3e
cglotr <amir.ariffin.920404@gmail.com>
2018-06-11 22:43:56 +0800

    add currency model tests

--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -3,4 +3,13 @@
 require "rails_helper"
 
 RSpec.describe Currency, type: :model do
+  it { should validate_length_of(:code).is_at_least 1 }
+
+  it { should validate_presence_of :code }
+  it { should validate_presence_of :created_at }
+  it { should validate_presence_of :updated_at }
+
+  it { should have_many :spendings }
+
+  it { should validate_uniqueness_of :code }
 end
