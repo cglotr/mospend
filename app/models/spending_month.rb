e6301fa0562227c028d8540dfd03c0e2a133c663
cglotr <amir.ariffin.920404@gmail.com>
2018-06-11 23:22:17 +0800

    add spending_month model spec

--- app/models/spending_month.rb
+++ app/models/spending_month.rb
@@ -3,4 +3,13 @@
 class SpendingMonth < ApplicationRecord
   belongs_to :user
   has_many :spendings, dependent: :destroy
+
+  validates_numericality_of :month
+  validates_numericality_of :year
+
+  validates_presence_of :created_at
+  validates_presence_of :month
+  validates_presence_of :updated_at
+  validates_presence_of :user_id
+  validates_presence_of :year
 end
---
+++ spec/models/spending_month_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe SpendingMonth, type: :model do
+  it { should belong_to :user }
+
+  it { should have_db_index :user_id }
+
+  it { should have_many(:spendings).dependent :destroy }
+
+  it { should validate_numericality_of :month }
+  it { should validate_numericality_of :year }
+
+  it { should validate_presence_of :created_at }
+  it { should validate_presence_of :month }
+  it { should validate_presence_of :updated_at }
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :year }
+end
