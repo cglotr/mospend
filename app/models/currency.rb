9383c6529f0837dbce2ca079c2ced957a503ed27
cglotr <amir.ariffin.920404@gmail.com>
2018-06-11 22:50:48 +0800

    passing currency model test

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -2,4 +2,8 @@
 
 class Currency < ApplicationRecord
   has_many :spendings
+
+  validates :code, length: { minimum: 1 }, presence: true, uniqueness: true
+  validates :created_at, presence: true
+  validates :updated_at, presence: true
 end
